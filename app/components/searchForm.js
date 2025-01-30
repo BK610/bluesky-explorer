6ea9c51c7bc5380688be734508cdb59b379a5419
BK610 <bailey.orion.kane@gmail.com>
2025-01-30 17:57:08 -0500

    Update styling

--- app/components/searchForm.js
+++ app/components/searchForm.js
@@ -9,24 +9,24 @@ const SearchForm = ({ onSearch, loading, searchError }) => {
   };
 
   return (
-    <div className="m-4 px-2 pt-1 pb-1 border border-gray-600 rounded-md w-full sm:w-2/3 mx-auto sticky z-20 top-4 bg-gray-100 dark:bg-gray-800 ">
+    <div className="m-4 px-3 pt-1 pb-1 border border-slate-600 dark:border-slate-400 rounded-md w-full sm:w-2/3 mx-auto sticky z-20 top-4 bg-sky-50 dark:bg-slate-900 shadow-md hover:shadow-lg">
       <form onSubmit={handleSearch}>
         <div
           className={`${
             loading || "invisible"
-          } mt-1 mx-auto text-sm text-gray-700 dark:text-gray-300`}
+          } mt-1 mx-auto text-sm text-slate-700 dark:text-slate-300`}
         >
           {loading ? "Loading..." : "Loaded"}
         </div>
         <div className="w-full flex">
           <input
-            className="rounded-l-lg w-full px-2 py-2 text-sm text-black font-mono border border-r-0 dark:border-white border-black"
+            className="rounded-l-md w-full px-2 py-2 text-sm text-black font-mono border border-r-0 dark:border-white border-black"
             type="search"
             placeholder="baileykane.co"
             onChange={(e) => setQuery(e.target.value)}
           />
           <button
-            className="px-2 py-2 font-semibold border dark:border-white border-black rounded-r-lg"
+            className="px-2 py-2 font-semibold border dark:border-white border-black rounded-r-md"
             type="submit"
           >
             Explore

e6acca0f57ead6bf234e8d248330dc008ef287ce
BK610 <bailey.orion.kane@gmail.com>
2025-02-11 22:49:10 -0500

    Add pageExtensions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+import createMDX from "@next/mdx";
+import rehypeExpressiveCode from "rehype-expressive-code";
+
+/** @type {import('rehype-expressive-code').RehypeExpressiveCodeOptions} */
+const rehypeExpressiveCodeOptions = {};
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [
+      // The nested array structure is required to pass options
+      // to a rehype plugin
+      [rehypeExpressiveCode, rehypeExpressiveCodeOptions],
+    ],
+  },
+});
 
 export default nextConfig;

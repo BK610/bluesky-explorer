e33fd7d2a6619bf2f6ebd466e989307f71bea131
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-14 11:06:11 -0400

    Add footer

--- app/layout.js
+++ app/layout.js
@@ -8,8 +8,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased bg-sky-100 dark:bg-slate-800`}>
-        {children}
+      <body
+        className={`antialiased bg-sky-100 dark:bg-slate-800 flex flex-col min-h-svh`}
+      >
+        <main className="flex-1">{children}</main>
+        <footer className="text-sm w-full text-center py-4 h-fit">
+          Made by{" "}
+          <a
+            href="https://www.baileykane.co/?ref=blueskyexplorer"
+            target="_blank"
+            className="underline hover:decoration-2"
+          >
+            Bailey Kane
+          </a>
+          , with curiosity.
+        </footer>
       </body>
     </html>
   );
